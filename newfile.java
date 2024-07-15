8c51ea42628a87ac73bf44e7166d4d7f4560c2a9
archita6363 <m6archita@gmail.com>
2024-07-15 17:42:03 +0530

    new file added to a new branch

--- newfile.java
+++ newfile.java
@@ -6,7 +6,7 @@ class Simple{
 
 class Simple{  
     public static void main(String args[]){  
-     System.out.println("Hello Git");  
+     System.out.println("Hello Git lovers");  
     }  
 }  
 
