e7331a77b839c7421344c3b81812b97e96a8806f
archita6363 <m6archita@gmail.com>
2024-07-15 19:05:50 +0530

    modified

--- newfile.java
+++ newfile.java
@@ -5,6 +5,6 @@ class Simple{
 }  
 class Simple{  
     public static void main(String args[]){  
-     System.out.println("Hello Git");  
+     System.out.println("Hello Git like");  
     }  
 }  
